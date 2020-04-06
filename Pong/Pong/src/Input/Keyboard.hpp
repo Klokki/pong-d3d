baff1f10c1ea7599579fc01d67ca0d791f786763
Sami Kivinen <21125844+Klokki@users.noreply.github.com>
2020-04-06 21:45:40 +0300

    make some functions const

--- Pong/Pong/src/Input/Keyboard.hpp
+++ Pong/Pong/src/Input/Keyboard.hpp
@@ -3,19 +3,19 @@
 #include "KeyboardEvent.hpp"
 
 // Keyboard.hpp
-// Contains the state of the keyboard (whether a specific key is pressed, etc)
+// Contains and modifies the state of the keyboard (whether a specific key is pressed, etc)
 
 class Keyboard
 {
 public:
 	Keyboard(){}
-	bool KeyPressed(const unsigned char keycode) { return m_keys[keycode]; }
-	bool KeyBufferEmpty() { return m_keyBuffer.empty(); }
+	bool KeyPressed(const unsigned char keycode) const { return m_keys[keycode]; }
+	bool KeyBufferEmpty() const { return m_keyBuffer.empty(); }
 	KeyboardEvent ReadBuffer();
 	void KeyDown(const unsigned char key);
 	void KeyReleased(const unsigned char key);
 	void SetAutoRepeat(bool value) { m_autoRepeat = value; }
-	bool IsAutoRepeat() { return m_autoRepeat; }
+	bool IsAutoRepeat() const { return m_autoRepeat; }
 private:
 	bool m_autoRepeat = false;
 	bool m_keys[256] = { false }; // initialize all keys to false
