11574bde61280a5f01afffefe9228e8036a7bb72
Sami Kivinen <21125844+Klokki@users.noreply.github.com>
2020-04-11 22:10:15 +0300

    move gameobject update out of header

--- Pong/Pong/src/Game/GameObject.cpp
+++ Pong/Pong/src/Game/GameObject.cpp
@@ -7,3 +7,11 @@ GameObject::GameObject(DirectX::XMFLOAT2 position, DirectX::XMFLOAT2 size)
 	m_size(size)
 {
 }
+
+void GameObject::Update(unsigned char keycode)
+{
+	if (keycode == 0x41 && m_position.x > m_size.x / 2)
+		m_position.x -= 5.f;
+	if (keycode == 0x44 && m_position.x < 800.f - m_size.x / 2)
+		m_position.x += 5.f;
+}
--- Pong/Pong/src/Game/GameObject.hpp
+++ Pong/Pong/src/Game/GameObject.hpp
@@ -6,12 +6,7 @@ class GameObject
 {
 public:
 	GameObject(DirectX::XMFLOAT2 position, DirectX::XMFLOAT2 size);
-	void Update(unsigned char keycode) {
-		if (keycode == 0x41 && m_position.x > 0.0f)
-			m_position.x -= 5.f;
-		if (keycode == 0x44 && m_position.x < 800.f)
-			m_position.x += 5.f;
-	}
+	void Update(unsigned char keycode);
 	void Draw(Renderer& renderer) { renderer.Render(m_position, m_size); }
 private:
 	DirectX::XMFLOAT2 m_position;
