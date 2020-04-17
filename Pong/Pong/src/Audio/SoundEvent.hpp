b1b7411d25a1cc732c14d2fedd22e1f4fabbcabb
Sami Kivinen <21125844+Klokki@users.noreply.github.com>
2020-04-17 04:01:12 +0300

    fix minor compiler warning

--- Pong/Pong/src/Audio/SoundEvent.hpp
+++ Pong/Pong/src/Audio/SoundEvent.hpp
@@ -10,9 +10,9 @@ public:
 	~SoundEvent(){}
 private:
 	IXAudio2SourceVoice* sourceVoice = nullptr;
-	WAVEFORMATEX waveFormat;
+	WAVEFORMATEX waveFormat = { 0 };
 	unsigned int waveLength = NULL;
 	std::vector<BYTE> audioData = { NULL };
-	XAUDIO2_BUFFER audioBuffer;
+	XAUDIO2_BUFFER audioBuffer = { 0 };
 	unsigned int index = 0;
 };
