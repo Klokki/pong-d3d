4aa9e4ed394411de23e07528b810587a1006d2ec
Sami Kivinen <21125844+Klokki@users.noreply.github.com>
2020-05-08 07:30:01 +0300

    remove redundant struct

--- Pong/Pong/shaders/Pixel.hlsl
+++ Pong/Pong/shaders/Pixel.hlsl
@@ -1,10 +1,4 @@
-struct PS_INPUT
+float4 main(float4 inPosition : SV_POSITION, float3 inColor : COLOR) : SV_TARGET
 {
-	float4 inPosition : SV_POSITION;
-	float3 inColor : COLOR;
-};
-
-float4 main(PS_INPUT input) : SV_TARGET
-{
-	return float4(input.inColor, 1.0f);
+	return float4(inColor, 1.0f);
 }
--- Pong/Pong/shaders/Vertex.hlsl
+++ Pong/Pong/shaders/Vertex.hlsl
@@ -5,27 +5,18 @@ cbuffer constantBuffer : register(b0)
 	float3 color;
 }
 
-struct VS_INPUT
-{
-	float3 inPos : POSITION;
-	float3 inColor : COLOR;
-};
-
 struct VS_OUTPUT
 {
 	float4 outPosition : SV_POSITION;
 	float3 outColor : COLOR;
 };
 
-VS_OUTPUT main(VS_INPUT input)
+VS_OUTPUT main(float3 inPos : POSITION, float3 inColor : COLOR)
 {
 	VS_OUTPUT output;
 
-	// set position
-	output.outPosition = mul(float4(input.inPos, 1.0f), mul(move, projection));
-
-	// set color
-	output.outColor = input.inColor * color;
+	output.outPosition = mul(float4(inPos, 1.0f), mul(move, projection));
+	output.outColor = inColor * color;
 
 	return output;
 }
